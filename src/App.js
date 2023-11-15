15326a695f4780fe1b75554da901f41f25210eac
MegablasterMan1 <tryjhnsn12@gmail.com>
2023-11-15 17:12:57 -0600

    Ran into a problem defining { App }

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useState, useEffect } from 'react';
+import { API } from 'aws-amplify';
+
+const App = () => {
+  // Create coins variable and set to empty array
+  const [coins, updateCoins] = useState([])
+
+  // Define function to all API
+  async function fetchCoins() {
+    const data = await API.get('cryptoapi', '/coins')
+    updateCoins(data.coins)
+  }
+
+  // Call fetchCoins function when component loads
+  useEffect(() => {
+    fetchCoins()
+  }, [])
+
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <div className="App">
+      {
+        coins.map((coin, index) => (
+          <div key={index}>
+            <h2>{coin.name} - {coin.symbol}</h2>
+            <h5>${coin.price_usd}</h5>
+          </div>
+        ))
+      }
+    </div>
   );
 }
 
