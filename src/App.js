d8f725f256c7cc6b70b88553f1340f2be938cdc5
MegablasterMan1 <tryjhnsn12@gmail.com>
2023-12-15 13:41:54 -0600

    Updating the Client App

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import { API } from 'aws-amplify';
 
 const App = () => {
   // Create coins variable and set to empty array
-  const [coins, updateCoins] = useState([])
+  const [coins, updateCoins] = useState([]);
+
+  // Create additional state to hold user input for limit and start properties
+  const [input, updateInput] = useState({ limit: 5, start: 0 });
+
+  // Create a new function to allow users to update the input values
+  function updateInputValues(type, value) {
+    updateInput({ ...input, [type]: value });
+  }
 
   // Define function to all API
   const fetchCoins = async() => {
-    const data = await API.get('cryptoapi', '/coins')
-    updateCoins(data.coins)
+    const { limit, start } = input;
+    const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`);
+    updateCoins(data.coins);
   }
 
   // Call fetchCoins function when component loads
@@ -20,6 +29,18 @@ const App = () => {
 
   return (
     <div className="App">
+      {/* Add input fields to the UI for user input */}
+        <input
+          placeholder="start"
+          onChange={e => updateInputValues('start', (e.target.value - 1))}
+        />
+        <input
+          onChange={e => updateInputValues('limit', e.target.value)}
+          placeholder="limit"
+        />
+
+      {/* Add button to the UI to give user the option to call the API */}
+        <button onClick={fetchCoins}>Fetch Coins</button>
       {
         coins.map((coin, index) => (
           <div key={index}>
