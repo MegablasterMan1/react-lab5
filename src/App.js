d7d7f3ecd396e15db75219cbd91f1d5bf08a41a1
MegablasterMan1 <tryjhnsn12@gmail.com>
2023-12-15 13:47:18 -0600

    One more use state demo. (Loading)

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ const App = () => {
   // Create additional state to hold user input for limit and start properties
   const [input, updateInput] = useState({ limit: 5, start: 0 });
 
+  //Create a variable for loading
+  const [loading, updateloading] = useState(true);
+
   // Create a new function to allow users to update the input values
   function updateInputValues(type, value) {
     updateInput({ ...input, [type]: value });
@@ -17,9 +20,11 @@ const App = () => {
 
   // Define function to all API
   const fetchCoins = async() => {
+    updateloading(true);
     const { limit, start } = input;
     const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`);
     updateCoins(data.coins);
+    updateloading(false);
   }
 
   // Call fetchCoins function when component loads
@@ -41,8 +46,9 @@ const App = () => {
 
       {/* Add button to the UI to give user the option to call the API */}
         <button onClick={fetchCoins}>Fetch Coins</button>
+        {loading && <h2>Loading ...</h2>}
       {
-        coins.map((coin, index) => (
+        !loading && coins.map((coin, index) => (
           <div key={index}>
             <h2>{coin.name} - {coin.symbol}</h2>
             <h5>${coin.price_usd}</h5>
